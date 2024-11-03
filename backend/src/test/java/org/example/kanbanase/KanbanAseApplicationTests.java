37652ca05e96eda2457b0efdb5de923cbc6dff40
Sarithagaganam999 <sarithagaganam2000@gmail.com>
2024-11-02 20:24:55 -0600

    feat: init backend

---
+++ backend/src/test/java/org/example/kanbanase/KanbanAseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kanbanase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanAseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
